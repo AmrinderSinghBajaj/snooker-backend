c31afa5c00dbfea47fd07e7e018d6f5aff3b5634
Amrinder Singh Bajaj <amrinder5397@gmail.com>
2026-07-19 17:25:59 +0530

    temp update test_db.js

--- backend/test_db.js
+++ backend/test_db.js
@@ -1,13 +1,19 @@
 import 'dotenv/config';
 import { connectDB } from './src/db.js';
 import Club from './src/models/Club.js';
+import AdminUser from './src/models/AdminUser.js';
+import Asset from './src/models/Asset.js';
+import Customer from './src/models/Customer.js';
+import GameSession from './src/models/GameSession.js';
 
 async function run() {
   await connectDB();
-  const clubs = await Club.find({});
-  console.log('CLUBS_DATA_START');
-  console.log(JSON.stringify(clubs, null, 2));
-  console.log('CLUBS_DATA_END');
+  console.log('--- COLLECTION COUNTS ---');
+  console.log('Clubs:', await Club.countDocuments({}));
+  console.log('AdminUsers:', await AdminUser.countDocuments({}));
+  console.log('Assets:', await Asset.countDocuments({}));
+  console.log('Customers:', await Customer.countDocuments({}));
+  console.log('GameSessions:', await GameSession.countDocuments({}));
   process.exit(0);
 }
 
